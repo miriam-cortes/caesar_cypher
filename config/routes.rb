67556afeb6d3773eda9b790e144d4a9092e79e10
Miriam Cortes <mcortes.umiami@gmail.com>
2016-09-27 16:26:26 -0700

    decode and encode both working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'decodings/index'
+
+  get 'decodings/new'
+
+  post 'decodings/show'
+  get 'decodings/show'
+
+
   get 'encodings/index'
 
   get 'encodings/new'
